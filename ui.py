e23b2cd6fb72af0b395224b1dfbe7983c633cad1
miraship <pov.mahmoudmira@outlook.com>
2026-07-25 14:40:50 +0300

    fix: fixed input steps and added loading state for better ux

--- ui.py
+++ ui.py
@@ -11,8 +11,8 @@ with st.form("predicition_form"):
 
     with col1:
         area = st.number_input("Area")
-        bedrooms = st.number_input("No of bedrooms")
-        bathrooms = st.number_input("No of bathrooms")
+        bedrooms = st.number_input("No of bedrooms", step=1)
+        bathrooms = st.number_input("No of bathrooms", step=1)
         furnishingstatus = st.selectbox(
             "Furnishing Status", ["Unfurnished", "Semi-Furnished", "Furnished"]
         )
@@ -27,28 +27,29 @@ with st.form("predicition_form"):
         parking = int(st.toggle("Has parking?"))
 
     if submitButton:
-        furnishingstatus = {"Unfurnished": 0, "Semi-Furnished": 1, "Furnished": 2}[
-            furnishingstatus
-        ]
+        with st.spinner("Predicting house price"):
+            furnishingstatus = {"Unfurnished": 0, "Semi-Furnished": 1, "Furnished": 2}[
+                furnishingstatus
+            ]
 
-        payload = {
-            "area": area,
-            "bedrooms": bedrooms,
-            "bathrooms": bathrooms,
-            "mainroad": mainroad,
-            "basement": basement,
-            "stories": 0,
-            "guestroom": guestroom,
-            "hotwaterheating": hotwaterheating,
-            "parking": parking,
-            "airconditioning": airconditioning,
-            "furnishingstatus": furnishingstatus,
-            "prefarea": 0,
-        }
+            payload = {
+                "area": area,
+                "bedrooms": bedrooms,
+                "bathrooms": bathrooms,
+                "mainroad": mainroad,
+                "basement": basement,
+                "stories": 0,
+                "guestroom": guestroom,
+                "hotwaterheating": hotwaterheating,
+                "parking": parking,
+                "airconditioning": airconditioning,
+                "furnishingstatus": furnishingstatus,
+                "prefarea": 0,
+            }
 
-        response = requests.post("http://localhost:8000/ai/predict", json=payload)
+            response = requests.post("http://localhost:8000/ai/predict", json=payload)
 
-        if response.status_code == 200 and response.json():
-            st.success(f"House Price: {response.json()['house_price']}")
-        else:
-            st.error("Internal Server Error")
+            if response.status_code == 200 and response.json():
+                st.success(f"House Price: {response.json()['house_price']}")
+            else:
+                st.error("Internal Server Error")
